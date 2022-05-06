07c9945e92ab5db915dfed9a2d72b4669dbe4b62
Uyenvy Nguyen <uyenvy521@revature.net>
2022-05-06 09:44:00 -0700

    added logo

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../Styles/Header.css';
 import {Link} from 'react-router-dom';
+import Logo from '../assets/logo.png';
 
 function Header() {
     return(
         <header id='head'>
-            <Link to='/'><h1 id='siteTitle'>The Bankers</h1></Link>
+            <Link to='/'><img src={Logo} width='140px' height='100px' /></Link>
 
             <nav>
                 <ul id='navBar'>
