62a6e8ad3f73acff9fed8deb71015069d3cec0ae
Marquel Ellis <100174037+marquel08@users.noreply.github.com>
2022-05-08 18:59:22 -0500

    updated the backend and axios page

--- backend/src/main/java/com/bankingapp/backend/controller/UserController.java
+++ backend/src/main/java/com/bankingapp/backend/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.bankingapp.backend.model.User;
 import com.bankingapp.backend.service.UserService;
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8081")
 
 
 @RestController
